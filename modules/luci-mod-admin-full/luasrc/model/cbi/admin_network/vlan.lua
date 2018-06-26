lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

commit 4c950f1f21941620be476737f8099a99044ba171

luci-mod-admin-full: fix possible switch status layout bug
In some cases we might get status information for more ports than which are
actually usable, prevent overflowing the port status row in this case.

Reported-at: https://forum.lede-project.org/t/x/15897/14
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-admin-full/luasrc/view/admin_network/switch_status.htm
@@ -35,7 +35,7 @@
 					{
 						var th = tr.childNodes[j+1];
 
-						if (!th)
+						if (!th || !th.hasAttribute('data-name'))
 							continue;
 
 						if (ports[j].link)

commit 342d3a2797a7bd821482eacbbcdf695816f2c699

luci-mod-admin-full: properly remap ports in switch status display
Fixes: 4c950f1f2 ("luci-mod-admin-full: fix possible switch status layout bug")
Fixes: 05c0e1956 ("luci-mod-admin-full: cleanup markup")
Reported-at: https://forum.lede-project.org/t/x/15897/19
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-admin-full/luasrc/model/cbi/admin_network/vlan.lua
@@ -260,7 +260,7 @@ m.uci:foreach("network", "switch",
 		end
 
 
-		local vid = s:option(Value, has_vlan4k or "vlan", "VLAN ID", "<div id='portstatus-%s'></div>" % switch_name)
+		local vid = s:option(Value, has_vlan4k or "vlan", "VLAN ID")
 		local mx_vid = has_vlan4k and 4094 or (num_vlans - 1)
 
 		vid.rmempty = false
@@ -333,7 +333,7 @@ m.uci:foreach("network", "switch",
 
 		local _, pt
 		for _, pt in ipairs(topo.ports) do
-			local po = s:option(ListValue, tostring(pt.num), pt.label, '<div id="portstatus-%s-%d"></div>' %{ switch_name, pt.num })
+			local po = s:option(ListValue, tostring(pt.num), pt.label)
 
 			po:value("",  translate("off"))
 

modules/luci-mod-admin-full/luasrc/view/admin_network/switch_status.htm
@@ -33,9 +33,9 @@
 				{
 					for (var j = 0; j < ports.length; j++)
 					{
-						var th = tr.childNodes[j+1];
+						var th = tr.querySelector('[data-name="%d"]'.format(j));
 
-						if (!th || !th.hasAttribute('data-name'))
+						if (!th)
 							continue;
 
 						if (ports[j].link)