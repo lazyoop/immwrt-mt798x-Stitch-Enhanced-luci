lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

commit 2015af220c455ee39bf521356f58262be7423e5c

address: Replace ualarm with setitimer
ualarm has been deprecated and may not be available in the libc.

Signed-off-by: Rosen Penev <rosenp@gmail.com>

libs/luci-lib-nixio/src/address.c
@@ -24,6 +24,7 @@
 
 #ifdef __linux__
 
+#include <sys/time.h>
 #include <signal.h>
 #include <setjmp.h>
 #include <unistd.h>
@@ -287,6 +288,7 @@ static int nixio_getnameinfo(lua_State *L) {
 	const char *family = luaL_optstring(L, 2, NULL);
 
 #ifdef __linux__
+	const struct itimerval t = { {timeout * 1000 * 1000, 0} , {0, 0} };
 	struct sigaction sa_new, sa_old;
 	int timeout = luaL_optnumber(L, 3, 0);
 	if (timeout > 0 && timeout < 1000)
@@ -308,7 +310,7 @@ static int nixio_getnameinfo(lua_State *L) {
 			return 3;
 		}
 
-		ualarm(timeout * 1000, 0);
+		setitimer(ITIMER_REAL, &t, NULL);
 	}
 #endif
 
@@ -339,7 +341,7 @@ static int nixio_getnameinfo(lua_State *L) {
 #ifdef __linux__
 	if (timeout > 0 && timeout < 1000)
 	{
-		ualarm(0, 0);
+		alarm(0);
 		sigaction(SIGALRM, &sa_old, NULL);
 	}
 #endif

commit bd8d81a9c480ccd72b495cd2715f0cf34a82c70b

luci-lib-nixio: fix compilation
Fixes: #2921
Fixes: 2015af220 ("address: Replace ualarm with setitimer")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

libs/luci-lib-nixio/src/address.c
@@ -288,9 +288,9 @@ static int nixio_getnameinfo(lua_State *L) {
 	const char *family = luaL_optstring(L, 2, NULL);
 
 #ifdef __linux__
-	const struct itimerval t = { {timeout * 1000 * 1000, 0} , {0, 0} };
 	struct sigaction sa_new, sa_old;
 	int timeout = luaL_optnumber(L, 3, 0);
+	const struct itimerval t = { {timeout * 1000 * 1000, 0} , {0, 0} };
 	if (timeout > 0 && timeout < 1000)
 	{
 		sa_new.sa_handler = nixio__handle_alarm;