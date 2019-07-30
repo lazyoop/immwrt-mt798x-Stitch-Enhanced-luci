lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

commit dddca561e1dc8661cb134f795f3c80e1aa56cfd5

luci-base: ui.js: save filled out but not added dynlist items on submit
Fixes: #2938
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/ui.js
@@ -1327,10 +1327,15 @@ var UIDynamicList = UIElement.extend({
 
 	getValue: function() {
 		var items = this.node.querySelectorAll('.item > input[type="hidden"]'),
+		    input = this.node.querySelector('.add-item > input[type="text"]'),
 		    v = [];
 
 		for (var i = 0; i < items.length; i++)
 			v.push(items[i].value);
+
+		if (input && input.value != null && input.value.match(/\S/) &&
+		    v.filter(function(s) { return s == input.value }).length == 0)
+			v.push(input.value);
 
 		return v;
 	},

commit 304b20861f450fdf4f152729792bea6f65383b59

luci-base: ui.js: do not save invalid dynlist values
Fixes: dddca561e ("luci-base: ui.js: save filled out but not added dynlist items on submit")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/ui.js
@@ -1334,6 +1334,7 @@ var UIDynamicList = UIElement.extend({
 			v.push(items[i].value);
 
 		if (input && input.value != null && input.value.match(/\S/) &&
+		    input.classList.contains('cbi-input-invalid') == false &&
 		    v.filter(function(s) { return s == input.value }).length == 0)
 			v.push(input.value);
 