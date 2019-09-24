lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

commit ce4b8527327826a852965f48175936ecd1c30d7a

luci-mod-system: ensure that textarea contents are properly escaped
Fixes: #3090
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-system/htdocs/luci-static/resources/view/system/startup.js
@@ -125,7 +125,7 @@ return L.view.extend({
 				]),
 				E('div', { 'data-tab': 'rc', 'data-tab-title': _('Local Startup') }, [
 					E('p', {}, _('This is the content of /etc/rc.local. Insert your own commands here (in front of \'exit 0\') to execute them at the end of the boot process.')),
-					E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 20 }, rcLocal != null ? rcLocal : '')),
+					E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 20 }, [ (rcLocal != null ? rcLocal : '') ]),
 					E('div', { 'class': 'cbi-page-actions' }, [
 						E('button', {
 							'class': 'btn cbi-button-save',

modules/luci-mod-system/htdocs/luci-static/resources/view/system/crontab.js
@@ -40,7 +40,7 @@ return L.view.extend({
 			E('p', {},
 				_('This is the system crontab in which scheduled tasks can be defined.') +
 				_('<br/>Note: you need to manually restart the cron service if the crontab file was empty before editing.')),
-			E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 10 }, crontab != null ? crontab : ''))
+			E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 10 }, [ crontab != null ? crontab : '' ]))
 		]);
 	},
 

commit 13abdbce2d0f5e5d3141233bcd20373f230af490

luci-mod-system: startup.js: fix syntax error
Fixes: ce4b85273 ("luci-mod-system: ensure that textarea contents are properly escaped")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-system/htdocs/luci-static/resources/view/system/startup.js
@@ -125,7 +125,7 @@ return L.view.extend({
 				]),
 				E('div', { 'data-tab': 'rc', 'data-tab-title': _('Local Startup') }, [
 					E('p', {}, _('This is the content of /etc/rc.local. Insert your own commands here (in front of \'exit 0\') to execute them at the end of the boot process.')),
-					E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 20 }, [ (rcLocal != null ? rcLocal : '') ]),
+					E('p', {}, E('textarea', { 'style': 'width:100%', 'rows': 20 }, [ (rcLocal != null ? rcLocal : '') ])),
 					E('div', { 'class': 'cbi-page-actions' }, [
 						E('button', {
 							'class': 'btn cbi-button-save',