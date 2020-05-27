lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

commit ce60fd7bb7fba2bf4ecf3bf4b2b3bc3c34164d9f

luci-proto-vxlan: add vxlan protol handler
Allow users to add vxlan inteface in luci.

Signed-off-by: Wojciech Jowsa <wojciech.jowsa@gmail.com>

protocols/luci-proto-vxlan/htdocs/luci-static/resources/protocol/vxlan.js
@@ -1,0 +1,84 @@
+'use strict';
+'require form';
+'require network';
+'require tools.widgets as widgets';
+
+network.registerPatternVirtual(/^vxlan-.+$/);
+
+return network.registerProtocol('vxlan', {
+	getI18n: function() {
+		return _('VXLAN (RFC7348)');
+	},
+
+	getIfname: function() {
+		return this._ubus('l3_device') || 'vxlan-%s'.format(this.sid);
+	},
+
+	getOpkgPackage: function() {
+		return 'vxlan';
+	},
+
+	isFloating: function() {
+		return true;
+	},
+
+	isVirtual: function() {
+		return true;
+	},
+
+	getDevices: function() {
+		return null;
+	},
+
+	containsDevice: function(ifname) {
+		return (network.getIfnameOf(ifname) == this.getIfname());
+	},
+
+	renderFormOptions: function(s) {
+		var o;
+
+		o = s.taboption('general', form.Value, 'remote', _('Remote IPv4 address'), _('The IPv4 address or the fully-qualified domain name of the remote end.'));
+		o.optional = false;
+		o.datatype = 'or(hostname,ip4addr("nomask"))';
+
+		o = s.taboption('general', form.Value, 'local', _('Local IPv4 address'), _('The local IPv4 address over which the tunnel is created (optional).'));
+		o.optional = true;
+		o.datatype = 'ip4addr("nomask")';
+
+		o = s.taboption('general', form.Value, 'id', _('VXLAN network identifier'), _('ID used to uniquely identify the VXLAN'));
+		o.optional = true;
+		o.datatype = 'range(1, 16777216)';
+
+		o = s.taboption('general', widgets.NetworkSelect, 'link', _('Bind interface'), _('Bind the tunnel to this interface (optional).'));
+		o.exclude = s.section;
+		o.nocreate = true;
+		o.optional = true;
+
+		o = s.taboption('advanced', form.Value, 'mtu', _('Override MTU'), _('Specify an MTU (Maximum Transmission Unit) other than the default (1280 bytes).'));
+		o.optional = true;
+		o.placeholder = 1280;
+		o.datatype = 'range(68, 9200)';
+
+		o = s.taboption('advanced', form.Value, 'ttl', _('Override TTL'), _('Specify a TTL (Time to Live) for the encapsulating packet other than the default (64).'));
+		o.optional = true;
+		o.placeholder = 64;
+		o.datatype = 'min(1)';
+
+		o = s.taboption('advanced', form.Value, 'tos', _('Override TOS'), _('Specify a TOS (Type of Service).'));
+		o.optional = true;
+		o.datatype = 'range(0, 255)';
+
+		o = s.taboption('advanced', form.Value, 'macaddr', _('Override MAC address'));
+		o.optional = true;
+		o.datatype = 'macaddr';
+
+		o = s.taboption('advanced', form.Flag, 'rxcsum', _('Enable rx checksum'));
+		o.optional = true;
+		o.default = o.enabled;
+
+		o = s.taboption('advanced', form.Flag, 'txcsum', _('Enable tx checksum'));
+		o.optional = true;
+		o.default = o.enabled;
+
+	}
+});

protocols/luci-proto-vxlan/htdocs/luci-static/resources/protocol/vxlan6.js
@@ -1,0 +1,84 @@
+'use strict';
+'require form';
+'require network';
+'require tools.widgets as widgets';
+
+network.registerPatternVirtual(/^vxlan-.+$/);
+
+return network.registerProtocol('vxlan6', {
+	getI18n: function() {
+		return _('VXLANv6 (RFC7348)');
+	},
+
+	getIfname: function() {
+		return this._ubus('l3_device') || 'vxlan-%s'.format(this.sid);
+	},
+
+	getOpkgPackage: function() {
+		return 'vxlan';
+	},
+
+	isFloating: function() {
+		return true;
+	},
+
+	isVirtual: function() {
+		return true;
+	},
+
+	getDevices: function() {
+		return null;
+	},
+
+	containsDevice: function(ifname) {
+		return (network.getIfnameOf(ifname) == this.getIfname());
+	},
+
+	renderFormOptions: function(s) {
+		var o;
+
+		o = s.taboption('general', form.Value, 'remote', _('Remote IPv6 address'), _('The IPv6 address or the fully-qualified domain name of the remote end.'));
+		o.optional = false;
+		o.datatype = 'or(hostname,cidr6)';
+
+		o = s.taboption('general', form.Value, 'local', _('Local IPv6 address'), _('The local IPv6 address over which the tunnel is created (optional).'));
+		o.optional = true;
+		o.datatype = 'cidr6';
+
+		o = s.taboption('general', form.Value, 'id', _('VXLAN network identifier'), _('ID used to uniquely identify the VXLAN'));
+		o.optional = true;
+		o.datatype = 'range(1, 16777216)';
+
+		o = s.taboption('general', widgets.NetworkSelect, 'link', _('Bind interface'), _('Bind the tunnel to this interface (optional).'));
+		o.exclude = s.section;
+		o.nocreate = true;
+		o.optional = true;
+
+		o = s.taboption('advanced', form.Value, 'mtu', _('Override MTU'), _('Specify an MTU (Maximum Transmission Unit) other than the default (1280 bytes).'));
+		o.optional = true;
+		o.placeholder = 1280;
+		o.datatype = 'range(68, 9200)';
+
+		o = s.taboption('advanced', form.Value, 'ttl', _('Override TTL'), _('Specify a TTL (Time to Live) for the encapsulating packet other than the default (64).'));
+		o.optional = true;
+		o.placeholder = 64;
+		o.datatype = 'min(1)';
+
+		o = s.taboption('advanced', form.Value, 'tos', _('Override TOS'), _('Specify a TOS (Type of Service).'));
+		o.optional = true;
+		o.datatype = 'range(0, 255)';
+
+		o = s.taboption('advanced', form.Value, 'macaddr', _('Override MAC address'));
+		o.optional = true;
+		o.datatype = 'macaddr';
+
+		o = s.taboption('advanced', form.Flag, 'rxcsum', _('Enable rx checksum'));
+		o.optional = true;
+		o.default = o.enabled;
+
+		o = s.taboption('advanced', form.Flag, 'txcsum', _('Enable tx checksum'));
+		o.optional = true;
+		o.default = o.enabled;
+
+	}
+});

protocols/luci-proto-vxlan/Makefile
@@ -1,0 +1,16 @@
+#
+# Copyright 2020 Wojciech Jowsa <wojciech.jowsa@gmail.com>
+#
+# This is free software, licensed under the Apache License, Version 2.0 .
+#
+
+include $(TOPDIR)/rules.mk
+
+LUCI_TITLE:=Support for Virtual eXtensible Local Area Network (VXLAN, RFC7348)
+LUCI_DEPENDS:=+vxlan
+
+PKG_MAINTAINER:=Wojciech Jowsa <wojciech.jowsa@gmail.com>
+
+include ../../luci.mk
+
+# call BuildPackage - OpenWrt buildroot signature

commit ab959ea7776ba42ef0e32be4222fae146203c38e

luci-proto-vxlan: fix uci option names
Fixes: ce60fd7bb ("luci-proto-vxlan: add vxlan protol handler")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

protocols/luci-proto-vxlan/htdocs/luci-static/resources/protocol/vxlan.js
@@ -37,19 +37,19 @@ return network.registerProtocol('vxlan', {
 	renderFormOptions: function(s) {
 		var o;
 
-		o = s.taboption('general', form.Value, 'remote', _('Remote IPv4 address'), _('The IPv4 address or the fully-qualified domain name of the remote end.'));
+		o = s.taboption('general', form.Value, 'peeraddr', _('Remote IPv4 address'), _('The IPv4 address or the fully-qualified domain name of the remote end.'));
 		o.optional = false;
 		o.datatype = 'or(hostname,ip4addr("nomask"))';
 
-		o = s.taboption('general', form.Value, 'local', _('Local IPv4 address'), _('The local IPv4 address over which the tunnel is created (optional).'));
+		o = s.taboption('general', form.Value, 'ipaddr', _('Local IPv4 address'), _('The local IPv4 address over which the tunnel is created (optional).'));
 		o.optional = true;
 		o.datatype = 'ip4addr("nomask")';
 
-		o = s.taboption('general', form.Value, 'id', _('VXLAN network identifier'), _('ID used to uniquely identify the VXLAN'));
+		o = s.taboption('general', form.Value, 'vid', _('VXLAN network identifier'), _('ID used to uniquely identify the VXLAN'));
 		o.optional = true;
 		o.datatype = 'range(1, 16777216)';
 
-		o = s.taboption('general', widgets.NetworkSelect, 'link', _('Bind interface'), _('Bind the tunnel to this interface (optional).'));
+		o = s.taboption('general', widgets.NetworkSelect, 'tunlink', _('Bind interface'), _('Bind the tunnel to this interface (optional).'));
 		o.exclude = s.section;
 		o.nocreate = true;
 		o.optional = true;

protocols/luci-proto-vxlan/htdocs/luci-static/resources/protocol/vxlan6.js
@@ -37,19 +37,19 @@ return network.registerProtocol('vxlan6', {
 	renderFormOptions: function(s) {
 		var o;
 
-		o = s.taboption('general', form.Value, 'remote', _('Remote IPv6 address'), _('The IPv6 address or the fully-qualified domain name of the remote end.'));
+		o = s.taboption('general', form.Value, 'peer6addr', _('Remote IPv6 address'), _('The IPv6 address or the fully-qualified domain name of the remote end.'));
 		o.optional = false;
 		o.datatype = 'or(hostname,cidr6)';
 
-		o = s.taboption('general', form.Value, 'local', _('Local IPv6 address'), _('The local IPv6 address over which the tunnel is created (optional).'));
+		o = s.taboption('general', form.Value, 'ip6addr', _('Local IPv6 address'), _('The local IPv6 address over which the tunnel is created (optional).'));
 		o.optional = true;
 		o.datatype = 'cidr6';
 
-		o = s.taboption('general', form.Value, 'id', _('VXLAN network identifier'), _('ID used to uniquely identify the VXLAN'));
+		o = s.taboption('general', form.Value, 'vid', _('VXLAN network identifier'), _('ID used to uniquely identify the VXLAN'));
 		o.optional = true;
 		o.datatype = 'range(1, 16777216)';
 
-		o = s.taboption('general', widgets.NetworkSelect, 'link', _('Bind interface'), _('Bind the tunnel to this interface (optional).'));
+		o = s.taboption('general', widgets.NetworkSelect, 'tunlink', _('Bind interface'), _('Bind the tunnel to this interface (optional).'));
 		o.exclude = s.section;
 		o.nocreate = true;
 		o.optional = true;