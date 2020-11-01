lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

commit 4acf12b4460cf307be16c0d19398e21d7cef230e

luci-mod-network: fix logic bug in parse enc for network join
In handleJoinConfirm while is_wep is a boolean, is_psk and is_sae are an array. In the following if check, all 3 are used as boolean but Js treat empty array as positive values and this cause the ui to wrongly set the encryption to sae. fix this by checking if the array actually contains data.

Fixes: #4524
Signed-off-by: Ansuel Smith <ansuelsmth@gmail.com>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1841,11 +1841,11 @@ return view.extend({
 					uci.set('wireless', section_id, 'bssid', bss.bssid);
 				}
 
-				if (is_sae) {
+				if (is_sae.length > 0) {
 					uci.set('wireless', section_id, 'encryption', 'sae');
 					uci.set('wireless', section_id, 'key', passval);
 				}
-				else if (is_psk) {
+				else if (is_psk.length > 0) {
 					for (var i = enc.wpa.length - 1; i >= 0; i--) {
 						if (enc.wpa[i] == 2) {
 							uci.set('wireless', section_id, 'encryption', 'psk2');

commit a4cd937b44aee4a357fd751dbb70191a1e8525f4

luci-mod-network: fix SAE/WPA check on network join
The method array may be null in case of an open network, also not all
occurrences of `is_sae` and `is_psk` were properly checked.

Resolve the issue by moving the length check to the variable initialization.

Fixes: 4acf12b44 ("luci-mod-network: fix logic bug in parse enc for network join")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1799,8 +1799,8 @@ return view.extend({
 			    zoneval = zoneopt ? zoneopt.formvalue('_new_') : null,
 			    enc = L.isObject(bss.encryption) ? bss.encryption : null,
 			    is_wep = (enc && Array.isArray(enc.wep)),
-			    is_psk = (enc && Array.isArray(enc.wpa) && L.toArray(enc.authentication).filter(function(a) { return a == 'psk' })),
-			    is_sae = (enc && Array.isArray(enc.wpa) && L.toArray(enc.authentication).filter(function(a) { return a == 'sae' }));
+			    is_psk = (enc && Array.isArray(enc.wpa) && L.toArray(enc.authentication).filter(function(a) { return a == 'psk' }).length > 0),
+			    is_sae = (enc && Array.isArray(enc.wpa) && L.toArray(enc.authentication).filter(function(a) { return a == 'sae' }).length > 0);
 
 			if (nameval == null || (passopt && passval == null))
 				return;
@@ -1841,11 +1841,11 @@ return view.extend({
 					uci.set('wireless', section_id, 'bssid', bss.bssid);
 				}
 
-				if (is_sae.length > 0) {
+				if (is_sae) {
 					uci.set('wireless', section_id, 'encryption', 'sae');
 					uci.set('wireless', section_id, 'key', passval);
 				}
-				else if (is_psk.length > 0) {
+				else if (is_psk) {
 					for (var i = enc.wpa.length - 1; i >= 0; i--) {
 						if (enc.wpa[i] == 2) {
 							uci.set('wireless', section_id, 'encryption', 'psk2');