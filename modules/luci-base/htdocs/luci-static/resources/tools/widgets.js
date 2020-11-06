lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

commit 48c3f19b0f99a9017e99b0185784727a2bea740f

luci-base: tools.widget.NetworkSelect: disallow invalid network names
Fixes: #4523, #4573
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/tools/widgets.js
@@ -393,6 +393,7 @@ var CBINetworkSelect = form.ListValue.extend({
 			select_placeholder: E('em', _('unspecified')),
 			display_items: this.display_size || this.size || 3,
 			dropdown_items: this.dropdown_size || this.size || 5,
+			datatype: 'uciname',
 			validate: L.bind(this.validate, this, section_id),
 			create: !this.nocreate,
 			create_markup: '' +

commit 2aaee110bf245628d58aab4d858ef7e9f06e5139

luci-base: tools.widgets.NetworkSelect: properly validate multi select
Fixes: 48c3f19b0 ("luci-base: tools.widget.NetworkSelect: disallow invalid network names")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/tools/widgets.js
@@ -393,7 +393,7 @@ var CBINetworkSelect = form.ListValue.extend({
 			select_placeholder: E('em', _('unspecified')),
 			display_items: this.display_size || this.size || 3,
 			dropdown_items: this.dropdown_size || this.size || 5,
-			datatype: 'uciname',
+			datatype: this.multiple ? 'list(uciname)' : 'uciname',
 			validate: L.bind(this.validate, this, section_id),
 			create: !this.nocreate,
 			create_markup: '' +