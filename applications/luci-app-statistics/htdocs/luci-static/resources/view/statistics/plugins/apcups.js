lazyoop/immwrt-mt798x-Stitch-Enhanced-luci

commit ef80396b85638ab1ab1a2618ce83436136aac459

luci-app-statistics: convert collectd configuration to client side views
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/apcups.js
@@ -1,0 +1,33 @@
+'use strict';
+'require form';
+
+return L.Class.extend({
+	title: _('APCUPS Plugin Configuration'),
+	description: _('The APCUPS plugin collects statistics about the APC UPS.'),
+
+	addFormOptions: function(s) {
+		var o;
+
+		o = s.option(form.Flag, 'enable', _('Enable this plugin'));
+		o.default = '0';
+
+		o = s.option(form.DynamicList, 'Host', _('Monitor host'));
+		o.default = 'localhost';
+		o.datatype = 'host';
+		o.depends('enable', '1');
+
+		o = s.option(form.Value, 'Port', _('Port for apcupsd communication'));
+		o.default = '3551';
+		o.datatype = 'port';
+		o.depends('enable', '1');
+	},
+
+	configSummary: function(section) {
+		var hosts = L.toArray(section.Host);
+		if (hosts.length)
+			return N_(hosts.length,
+				'Monitoring APC UPS at host %s, port %d',
+				'Monitoring APC UPS at hosts %s, port %d'
+			).format(hosts.join(', '), section.Port || 3551);
+	}
+});

commit bcd66557d518c816dd6ffc4a476e0450b231f127

luci-app-statistics: fix APC UPS host configuration
Ensure that list of hosts is stored as space separated list, like it was
done in the previous Lua implementation.

Fixes: #5010
Fixes: ef80396b8 ("luci-app-statistics: convert collectd configuration to client side views")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>
(cherry picked from commit d46eeb7970e74cb09a6655436079c6be28372cf5)

applications/luci-app-statistics/htdocs/luci-static/resources/view/statistics/plugins/apcups.js
@@ -15,6 +15,9 @@ return baseclass.extend({
 		o.default = 'localhost';
 		o.datatype = 'host';
 		o.depends('enable', '1');
+		o.write = function(section_id, value) {
+			return form.Value.prototype.write.call(this, section_id, L.toArray(value).join(' '));
+		};
 
 		o = s.option(form.Value, 'Port', _('Port for apcupsd communication'));
 		o.default = '3551';