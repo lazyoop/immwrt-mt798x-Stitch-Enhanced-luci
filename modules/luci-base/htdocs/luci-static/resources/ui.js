02196350ed99714f450b3048832a4e2466bc8ff3
Florian Eckert <fe@dev.tdt.de>
2024-06-07 09:45:25 +0200

    luci-base: replace button tag with div tag in ui.js
    Signed-off-by: Florian Eckert <fe@dev.tdt.de>
    (cherry picked from commit f83e68bea0f2590980a3befe5bc219209a7cf9a0)

--- modules/luci-base/htdocs/luci-static/resources/ui.js
+++ modules/luci-base/htdocs/luci-static/resources/ui.js
@@ -4220,7 +4220,7 @@ var UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 								upload.focus();
 							}
 						}),
-						E('button', {
+						E('div', {
 							'class': 'btn cbi-button',
 							'click': function(ev) {
 								ev.target.previousElementSibling.click();
@@ -4228,7 +4228,7 @@ var UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 						}, [ _('Browse…') ])
 					]),
 					E('div', { 'class': 'right', 'style': 'flex:1' }, [
-						E('button', {
+						E('div', {
 							'class': 'btn cbi-button',
 							'click': function() {
 								UI.prototype.hideModal();
@@ -4236,7 +4236,7 @@ var UI = baseclass.extend(/** @lends LuCI.ui.prototype */ {
 							}
 						}, [ _('Cancel') ]),
 						' ',
-						E('button', {
+						E('div', {
 							'class': 'btn cbi-button-action important',
 							'disabled': true,
 							'click': function(ev) {
